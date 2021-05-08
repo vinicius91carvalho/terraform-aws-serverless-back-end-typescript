7601a908a7c43fe1a685c1e86aca18595d633c8b
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-05-08 19:06:28 +0000

    Merged in hotfix/ci-cd (pull request #1)
    Fix pipeline configuration for branches: (hotfix, feature, and release)
    
    * fix: some configurations for stage and prod environment

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -4,6 +4,15 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "customer-api-645026403743-us-east-1-remote-state"
+    key            = "customer-api/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "customer-api-645026403743-us-east-1-remote-state"
+  }
+}
+
 module "auth" {
   source                   = "./auth"
   environment              = var.environment
