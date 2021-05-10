7cd496caec76d0eb4990259e6d29d88b34e7ac4d
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-05-10 18:40:30 -0300

    feat: ensure LoadCustomerByIdController calls LoadCustomerByIdUseCase with correct value

--- src/presentation/controllers/load-customer-by-id-controller.ts
+++ src/presentation/controllers/load-customer-by-id-controller.ts
@@ -1,17 +1,23 @@
 import { badRequest, serverError } from '@/presentation/helpers/http-helpers'
 import Controller from '@/presentation/protocols/controller-protocol'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http-protocols'
+import { LoadCustomerByIdUseCase } from '@/presentation/protocols/usecases/load-customer-by-id-use-case'
 import { Validator } from '@/validation/validator-protocol'
 
 export class LoadCustomerByIdController implements Controller {
-  constructor (private readonly loadCustomerByIdValidator: Validator<string>) {}
+  constructor (
+    private readonly loadCustomerByIdValidator: Validator<string>,
+    private readonly loadCustomerByIdUseCase: LoadCustomerByIdUseCase
+  ) {}
 
   async handle (httpRequest: HttpRequest<void>): Promise<HttpResponse> {
     try {
-      const schemaValidationError = await this.loadCustomerByIdValidator.validate(httpRequest.pathParameters.customerId)
+      const customerId = httpRequest.pathParameters.customerId
+      const schemaValidationError = await this.loadCustomerByIdValidator.validate(customerId)
       if (schemaValidationError) {
         return badRequest(schemaValidationError)
       }
+      await this.loadCustomerByIdUseCase.execute(customerId)
       return null
     } catch (error) {
       return serverError(error)
