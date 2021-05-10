cc019a51ed086d1c967735e4781b772666372c70
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-05-10 19:12:29 -0300

    feat: ensure LoadCustomerById calls FindCustomerByIdRepository with correct value

---
+++ src/usecases/load-customer-by-id.ts
@@ -0,0 +1,12 @@
+import { Customer } from '@/domain/customer'
+import { LoadCustomerByIdUseCase } from '@/presentation/protocols/usecases/load-customer-by-id-use-case'
+import { FindCustomerByIdRepository } from '@/usecases/protocols/find-customer-by-id-repository'
+
+export class LoadCustomerById implements LoadCustomerByIdUseCase {
+  constructor (private readonly findCustomerByIdRepository: FindCustomerByIdRepository) {}
+
+  async execute (customerId: string): Promise<Customer> {
+    await this.findCustomerByIdRepository.findById(customerId)
+    return null
+  }
+}
